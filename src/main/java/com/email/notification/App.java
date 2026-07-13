8a1412dd0bee71e81474a41ebdf65524fbce6b4f
MdMohibulHoque <mehebub81359@gmail.com>
2026-07-13 07:14:27 +0530

    Testing GitHub Webhook

--- src/main/java/com/email/notification/App.java
+++ src/main/java/com/email/notification/App.java
@@ -5,6 +5,6 @@ package com.email.notification;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Webhook Test");
     }
 }
