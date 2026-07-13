11d0ba38abe845a9472dd38e75b6a4703a387555
MdMohibulHoque <mehebub81359@gmail.com>
2026-07-13 07:18:32 +0530

    Test failed build

--- src/main/java/com/email/notification/App.java
+++ src/main/java/com/email/notification/App.java
@@ -5,6 +5,6 @@ package com.email.notification;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Webhook Test");
+        System.out.println("Testing failure")
     }
 }
